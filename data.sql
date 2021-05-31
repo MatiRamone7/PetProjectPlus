28d00885fdd4380bb1eb80ff038aaecc96f75edb
Mati <mati.leo@outlook.com>
2021-05-30 22:02:07 -0300

    Base de datos

--- data.sql
+++ data.sql
@@ -1,15 +1,35 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ong;
+DROP TABLE IF EXISTS usuario;
 
-CREATE TABLE users (
-  id INT PRIMARY KEY IDENTITY(1,1),
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  race VARCHAR(255) DEFAULT NULL
-);
+CREATE TABLE [ong] (
+	id int primary Key identity(1,1),
+	nombre varchar(255),
+	estandarizacionDeImagen varchar(255)
+)
 
-INSERT INTO users (first_name, last_name, race) VALUES
-  ('Commander', 'Shepard', 'Human'),
-  ('Garrus', 'Vakarian', 'Turian'),
-  ('Urdnot', 'Wrex', 'Krogan');
+INSERT INTO ong (nombre, estandarizacionDeImagen) VALUES
+  ('Pets Corp', 'Imagen Grande'),
+  ('Partners of Pets', 'Imagen Mediana'),
+  ('Pets Refugee', 'Imagen Chica');
+ 
+CREATE TABLE [usuario] (
+	id int primary Key identity(1,1),
+	nombre varchar(255),
+	password varchar(255),
+	apellido varchar(255),
+	fechaDeNacimiento DateTime2,
+	tipoDocumento varchar(255),
+	numeroDocumento int,
+	--FK
+	ongId int,
+	CONSTRAINT FK_ONG FOREIGN KEY (OngId) REFERENCES ONG(id)
+)
+
+INSERT INTO usuario (nombre, password, apellido, fechaDeNacimiento, tipoDocumento, numeroDocumento, ongId) VALUES
+  ('Commander', 'Pass1', 'Shepard', GetDate(), 'DNI', 38789348, 1),
+  ('Garrus', 'Pass2', 'Vakarian', GetDate(), 'DNI', 44344242, 2),
+  ('Urdnot', 'Pass3', 'Wrex', GetDate(), 'DNI', 23344444, 3),
+  ('Mordin', 'Pass4', 'Solus', GetDate(), 'DNI', 34379899, 1);
   
- select * from users
+ select * from ong;
+ select * from usuario;

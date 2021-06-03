011e9eb40dcbc9d72e10565b139527ad3818f942
Mati <mati.leo@outlook.com>
2021-06-02 22:26:43 -0300

    Base de datos para publicacion y rescatista

--- data.sql
+++ data.sql
@@ -22,7 +22,7 @@ CREATE TABLE [usuario] (
 	numeroDocumento int,
 	--FK
 	ongId int,
-	CONSTRAINT FK_ONG FOREIGN KEY (OngId) REFERENCES ONG(id)
+	CONSTRAINT FK_ONG FOREIGN KEY (ongId) REFERENCES ONG(id)
 )
 
 INSERT INTO usuario (nombre, password, apellido, fechaDeNacimiento, tipoDocumento, numeroDocumento, ongId) VALUES
@@ -30,6 +30,39 @@ INSERT INTO usuario (nombre, password, apellido, fechaDeNacimiento, tipoDocument
   ('Garrus', 'Pass2', 'Vakarian', GetDate(), 'DNI', 44344242, 2),
   ('Urdnot', 'Pass3', 'Wrex', GetDate(), 'DNI', 23344444, 3),
   ('Mordin', 'Pass4', 'Solus', GetDate(), 'DNI', 34379899, 1);
-  
+
+CREATE TABLE [rescatista] (
+	id int primary Key identity(1,1),
+	nombre varchar(255),
+	apellido varchar(255),
+	tipoDocumento varchar(255),
+	numeroDocumento int,
+	direccion varchar(255),
+	contacto varchar(255),
+)
+
+INSERT INTO rescatista (nombre, apellido, tipoDocumento, numeroDocumento, direccion, contacto) VALUES
+  ('Rescatador', 'De Ropes', 'DNI', 33523334, 'Elcano 3445', '11-5455-2345'),
+  ('Rescatador', 'De Togas', 'DNI', 23249876, 'Av. San Martin 235', 'Rescatador@hotmail.com'),
+  ('Encontré', 'Una tortuga', 'DNI', 3455234, 'Av. Corrientes 577', '4988-3244'),
+  ('Encontré', 'Un puma', 'DNI', 30665190, 'Florida 201', 'Rescatador@gmail.com');
+ 
+CREATE TABLE [publicacion] (
+	id int primary Key identity(1,1),
+	foto varchar(255),
+	lugarEncuentro varchar(255),
+	--FK
+	ongId int,
+	CONSTRAINT FK_ONG_FROM_PUBLICACION FOREIGN KEY (ongId) REFERENCES ONG(id),
+	rescatistaId int,
+	CONSTRAINT FK_RESCATISTA FOREIGN KEY (rescatistaId) REFERENCES rescatista(id)
+)
+
+INSERT INTO publicacion (foto, lugarEncuentro, ongId, rescatistaId) VALUES
+  ('Foto Perro', 'Encontrado en Villa Crespo', 1, 1),
+  ('Foto Gato', 'Encontrado en Plaza Miserere', 2, 2)
+
  select * from ong;
  select * from usuario;
+ select * from rescatista;
+ select * from publicacion;

dae59ee41f9b72fdba0a6845ea8acac290222f56
danimontesano <dmontesano@est.frba.utn.edu.ar>
2021-09-06 13:28:46 -0300

    Repo, service y controller de los otros formularios

--- PetProject++/src/main/java/com/utn/repositories/IFormRepo.java
+++ PetProject++/src/main/java/com/utn/repositories/IFormRepo.java
@@ -1,6 +1,9 @@
 package com.utn.repositories;
 
 
+import com.utn.models.forms.FormularioDarEnAdopcion;
+import com.utn.models.forms.FormularioIntencionAdopcion;
+import com.utn.models.forms.FormularioMascotaEncontrada;
 import com.utn.models.forms.FormularioMascotaPerdida;
 
 public interface IFormRepo {
@@ -13,21 +16,52 @@ public interface IFormRepo {
 
     public FormularioMascotaPerdida GetFormMascotaPerdidaById(Integer id);
 
-    public FormularioMascotaPerdida CreateFormMascotaPerdida(FormularioMascotaPerdida pet);
+    public FormularioMascotaPerdida CreateFormMascotaPerdida(FormularioMascotaPerdida form);
 
-    public FormularioMascotaPerdida UpdateFormMascotaPerdida(FormularioMascotaPerdida pet, Integer id);
+    public FormularioMascotaPerdida UpdateFormMascotaPerdida(FormularioMascotaPerdida form, Integer id);
 
     public void DeleteFormMascotaPerdida(Integer id);
+
 
     /*
         Form Mascota Encontrada
     */
 
+    public Iterable<FormularioMascotaEncontrada> GetFormsMascotaEncontrada();
+
+    public FormularioMascotaEncontrada GetFormMascotaEncontradaById(Integer id);
+
+    public FormularioMascotaEncontrada CreateFormMascotaEncontrada(FormularioMascotaEncontrada form);
+
+    public FormularioMascotaEncontrada UpdateFormMascotaEncontrada(FormularioMascotaEncontrada form, Integer id);
+
+    public void DeleteFormMascotaEncontrada(Integer id);
+
+
     /*
         Form Intencion Adopcion
     */
 
+    public Iterable<FormularioIntencionAdopcion> GetFormsIntencionAdopcion();
+
+    public FormularioIntencionAdopcion GetFormIntencionAdopcionById(Integer id);
+
+    public FormularioIntencionAdopcion CreateFormIntencionAdopcion(FormularioIntencionAdopcion form);
+
+    public FormularioIntencionAdopcion UpdateFormIntencionAdopcion(FormularioIntencionAdopcion form, Integer id);
+
+    public void DeleteFormIntencionAdopcion(Integer id);
+
     /*
         Form Dar Adopcion
     */
+    public Iterable<FormularioDarEnAdopcion> GetFormsDarEnAdopcion();
+
+    public FormularioDarEnAdopcion GetFormDarEnAdopcionById(Integer id);
+
+    public FormularioDarEnAdopcion CreateFormDarEnAdopcion(FormularioDarEnAdopcion form);
+
+    public FormularioDarEnAdopcion UpdateFormDarEnAdopcion(FormularioDarEnAdopcion form, Integer id);
+
+    public void DeleteFormDarEnAdopcion(Integer id);
 }
--- PetProject++/src/main/java/com/utn/services/IFormService.java
+++ PetProject++/src/main/java/com/utn/services/IFormService.java
@@ -1,5 +1,8 @@
 package com.utn.services;
 
+import com.utn.models.forms.FormularioDarEnAdopcion;
+import com.utn.models.forms.FormularioIntencionAdopcion;
+import com.utn.models.forms.FormularioMascotaEncontrada;
 import com.utn.models.forms.FormularioMascotaPerdida;
 
 public interface IFormService {
@@ -19,12 +22,40 @@ public interface IFormService {
     /*
         Form Mascota Encontrada
     */
+    public Iterable<FormularioMascotaEncontrada> GetFormsMascotaEncontrada();
+
+    public FormularioMascotaEncontrada GetFormMascotaEncontradaById(Integer id);
+
+    public FormularioMascotaEncontrada CreateFormMascotaEncontrada(FormularioMascotaEncontrada form);
+
+    public FormularioMascotaEncontrada UpdateFormMascotaEncontrada(FormularioMascotaEncontrada form, Integer id);
+
+    public void DeleteFormMascotaEncontrada(Integer id);
 
     /*
         Form Intencion Adopcion
     */
+    public Iterable<FormularioIntencionAdopcion> GetFormsIntencionAdopcion();
+
+    public FormularioIntencionAdopcion GetFormIntencionAdopcionById(Integer id);
+
+    public FormularioIntencionAdopcion CreateFormIntencionAdopcion(FormularioIntencionAdopcion form);
+
+    public FormularioIntencionAdopcion UpdateFormIntencionAdopcion(FormularioIntencionAdopcion form, Integer id);
+
+    public void DeleteFormIntencionAdopcion(Integer id);
+
 
     /*
         Form Dar Adopcion
     */
+    public Iterable<FormularioDarEnAdopcion> GetFormsDarEnAdopcion();
+
+    public FormularioDarEnAdopcion GetFormDarEnAdopcionById(Integer id);
+
+    public FormularioDarEnAdopcion CreateFormDarEnAdopcion(FormularioDarEnAdopcion form);
+
+    public FormularioDarEnAdopcion UpdateFormDarEnAdopcion(FormularioDarEnAdopcion form, Integer id);
+
+    public void DeleteFormDarEnAdopcion(Integer id);
 }

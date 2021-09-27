38167641c23fa88d15fd3c830e30a1759db19ae3
danimontesano <dmontesano@est.frba.utn.edu.ar>
2021-09-27 18:34:13 -0300

    .

--- PetProject++/src/main/java/com/utn/controllers/UserController.java
+++ PetProject++/src/main/java/com/utn/controllers/UserController.java
@@ -29,12 +29,13 @@ public class UserController {
     @GetMapping
     public String GetUsers() throws IOException {
 
-        Handlebars handlebars = new Handlebars();
-        Template template = handlebars.compile("formularioUsuario");
-        Map<String, Object> model = new HashMap<>();
-        model.put("usuarios", userService.GetUsers());
+        Handlebars handlebars = new Handlebars();       //se crea la instancia de handlebars
+        Template template = handlebars.compile("formularioUsuario");  //se crea el template sobre el .hbs que querés enviar (ej: formularioUsuario.hbs)
+        Map<String, Object> model = new HashMap<>();    //en este map se ponen todas las variables o clases que quieras usar luego en el handlebars
+        model.put("usuarios", userService.GetUsers());  //acá pones las cosas dentro del map (en este ejemplo estoy poniendo una lista de usuarios
+                                                        //con una etiqueta llama "usuarios)
 
-        return template.apply(model);
+        return template.apply(model);                   //aplicas las variables del template y las envías
     }
 
     @GetMapping("/{id}")

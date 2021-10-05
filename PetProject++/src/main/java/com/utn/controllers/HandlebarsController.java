2bc6c53fcb49b325df79a78c47761942670cb6e6
danimontesano <dmontesano@est.frba.utn.edu.ar>
2021-10-05 18:06:14 -0300

    un par de cambios en los .hbs

--- PetProject++/src/main/java/com/utn/controllers/HandlebarsController.java
+++ PetProject++/src/main/java/com/utn/controllers/HandlebarsController.java
@@ -7,6 +7,8 @@ import com.github.jknack.handlebars.io.TemplateLoader;
 import com.utn.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -14,18 +16,19 @@ import java.util.Map;
 
 //NOMENCLATURA PARA MAPPINGS
 //TODO: JSON-> en inglés, in english  // HTML -> en español, in spanish
-
+@RestController
+@RequestMapping("/")
 public class HandlebarsController {
 
     @Autowired
     IUserService userService;
 
     /**EJEMPLO DE COMO SINCRONIZAMOS HANDLEBARS CON SPRING*/
-    @GetMapping
+    @GetMapping("usuario")
     public String GetUsers() throws IOException {
         TemplateLoader loader = new ClassPathTemplateLoader("/templates", ".hbs");
         Handlebars handlebars = new Handlebars(loader);       //se crea la instancia de handlebars
-        Template template = handlebars.compile("perfil");  //se crea el template sobre el .hbs que querés enviar (ej: formularioUsuario.hbs)
+        Template template = handlebars.compile("mascotaPerdida");  //se crea el template sobre el .hbs que querés enviar (ej: formularioUsuario.hbs)
         Map<String, Object> model = new HashMap<>();    //en este map se ponen todas las variables o clases que quieras usar luego en el handlebars
         model.put("usuarios", userService.GetUsers());  //acá pones las cosas dentro del map (en este ejemplo estoy poniendo una lista de usuarios
         //con una etiqueta llama "usuarios)

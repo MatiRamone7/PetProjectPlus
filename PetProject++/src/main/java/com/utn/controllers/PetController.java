7936d0c8d92c2d52798e0dc2dd9a0a7b4b58687e
danimontesano <dmontesano@est.frba.utn.edu.ar>
2021-10-13 17:12:20 -0300

    fotos funcionando

--- PetProject++/src/main/java/com/utn/controllers/PetController.java
+++ PetProject++/src/main/java/com/utn/controllers/PetController.java
@@ -1,13 +1,19 @@
 package com.utn.controllers;
 
+import com.utn.models.forms.Foto;
+import com.utn.models.mascotas.Caracteristica;
+import com.utn.models.mascotas.CaracteristicaPet;
 import com.utn.models.mascotas.Mascota;
+import com.utn.services.ICaracteristicaService;
 import com.utn.services.IPetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 
 @RestController
 @RequestMapping("/pets")
@@ -15,6 +21,9 @@ public class PetController {
 
     @Autowired
     IPetService petService;
+
+    @Autowired
+    ICaracteristicaService caracteristicaService;
 
     @GetMapping
     public Iterable<Mascota> GetPets() {
@@ -27,14 +36,33 @@ public class PetController {
     }
 
     @PostMapping
-    public void CreatePet(@RequestParam Map<String, String> body, HttpServletResponse response) throws IOException {
+    public void CreatePet(@RequestParam Map<String, String> body, @RequestParam("imagen") MultipartFile img, HttpServletResponse response) throws IOException {
     Mascota mascota = new Mascota();
         mascota.setApodo(body.get("apodo"));
         mascota.setNombre(body.get("nombre"));
         mascota.setEspecie(Mascota.Especie.valueOf(body.get("especie")));
         mascota.setDescripcionFisica(body.get("descripcionFisica"));
         mascota.setSexo(Mascota.Sexo.valueOf(body.get("sexo")));
+        mascota.setFechaDeNacimiento(LocalDate.parse(body.get("fechaDeNacimiento")));
 
+        Iterable<Caracteristica> caracteristicas = caracteristicaService.GetCaracteristicas();
+        Set<CaracteristicaPet> caracteristicasPets = new HashSet<>();
+        for(Caracteristica caracteristica : caracteristicas){
+            if(!body.get(caracteristica.getDescripcion()).isEmpty()) {
+                CaracteristicaPet caracteristicaPet = new CaracteristicaPet();
+                Caracteristica caract = new Caracteristica();
+                caract.setId(caracteristica.getId());
+                caracteristicaPet.setTipoCaracteristica(caract);
+                caracteristicaPet.setValor(body.get(caracteristica.getDescripcion()));
+                caracteristicasPets.add(caracteristicaPet);
+            }
+        }
+        mascota.setCaracteristicSet(caracteristicasPets);
+
+        List<Foto> fotos = new ArrayList<>();
+        fotos.add(new Foto(img.getBytes(), img.getName()));
+        mascota.setFotos(fotos);
+        mascota.setDescripcionFisica(body.get("descripcionFisica"));
 
         petService.CreatePet(mascota);
 
--- PetProject++/src/main/java/com/utn/models/forms/Foto.java
+++ PetProject++/src/main/java/com/utn/models/forms/Foto.java
@@ -11,11 +11,44 @@ public class Foto {
     private Integer id;
 
     @Lob
-    @Column(name = "imagen", columnDefinition="VARBINARY(8000)")
+    @Column(name = "imagen", columnDefinition="VARBINARY(MAX)")
     public byte[] imagenByteArray;
 
     @Column(name = "descripcion")
     public String descripcion;
+
+    public Foto(byte[] imagenByteArray, String descripcion) {
+        this.imagenByteArray = imagenByteArray;
+        this.descripcion = descripcion;
+    }
+
+    public Foto() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public byte[] getImagenByteArray() {
+        return imagenByteArray;
+    }
+
+    public void setImagenByteArray(byte[] imagenByteArray) {
+        this.imagenByteArray = imagenByteArray;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
     public void describir(String ndescripcion){
         descripcion = ndescripcion;

e388ff75a6ff67b62419cc92dee4f4a9543d677f
joseVirgili <virgili.josemaria@gmail.com>
2021-10-13 21:02:30 -0300

    map create ong

--- PetProject++/src/main/java/com/utn/controllers/OngController.java
+++ PetProject++/src/main/java/com/utn/controllers/OngController.java
@@ -2,7 +2,9 @@ package com.utn.controllers;
 
 import com.utn.models.forms.Ciudad;
 import com.utn.models.forms.Direccion;
+import com.utn.models.forms.Foto;
 import com.utn.models.forms.Provincia;
+import com.utn.models.ongs.CharsFoto;
 import com.utn.models.ongs.Organizacion;
 import com.utn.models.users.ContactoUnico;
 import com.utn.models.users.Sesion;
@@ -11,10 +13,13 @@ import com.utn.models.users.Usuario;
 import com.utn.services.IOngService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -36,6 +41,38 @@ public class OngController {
 
     @PostMapping
     public void Create(@RequestParam Map<String, String> body, HttpServletResponse response) throws IOException {
+        Organizacion ong = new Organizacion();
+        ong.setNombre(body.get("nombre"));
+        CharsFoto especificacion = new CharsFoto();
+        especificacion.setSizeFoto(Integer.parseInt(body.get("sizeFoto")));
+        especificacion.setNivelCalidad(Integer.parseInt(body.get("nivelCalidad")));
+        ong.setEspecificacionesFotos(especificacion);
+
+
+
+        Direccion direccion = new Direccion();
+        direccion.setCalle(body.get("direccion.calle"));
+        direccion.setDepartamento(body.get("direccion.departamento"));
+        direccion.setCodigoPostal(body.get("direccion.codigoPostal"));
+
+        Ciudad ciudad = new Ciudad();
+        ciudad.setNombre(body.get("direccion.ciudad.nombre"));
+
+        Provincia provincia = new Provincia();
+        try{
+            direccion.setNumero(Integer.parseInt(body.get("direccion.numero")));
+            provincia.setId(Integer.valueOf(body.get("direccion.ciudad.provincia.id")));
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
+
+        ciudad.setProvincia(provincia);
+        direccion.setCiudad(ciudad);
+        ong.setDireccion(direccion);
+
+
+
         response.sendRedirect("/Inicio");
     }
 
